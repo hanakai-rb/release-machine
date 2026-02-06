5aadb7c4e42f1b3a1a4350888686590dc97487b6
Tim Riley <tim@riley.id.au>
2026-02-06 14:29:12 +1100

    Linkify GitHub resources in release note lines

--- .github/scripts/announce-to-forum.rb
+++ .github/scripts/announce-to-forum.rb
@@ -54,6 +54,52 @@ release_url = release_data["html_url"]
 release_body = release_data["body"] || ""
 version = release_data["tag_name"].sub(/^v/, "")
 
+# Linkify GitHub references in the release notes, but only within parentheses at the end of lines.
+# In our conventional CHANGELOG format, this is where we include GitHub references.
+#
+# Taking this approach means we don't have to worry about incorrect linking in other parts of the
+# release notes.
+
+# Matches "(@username in #15)" parentheticals at the end of our release note bullets.
+CLOSING_PARENS_REGEXP = /
+  ^        # start of line
+  (-.*)    # capture: dash and everything up to the opening paren
+  \(       # opening paren
+  ([^)]+)  # capture: content inside parentheses
+  \)       # closing paren
+  \s*      # optional trailing whitespace
+  $        # end of line
+/x
+
+# Matches "@username" mentions.
+GITHUB_USERNAME_REGEXP = /(?<!\w)@([a-zA-Z0-9](?:[a-zA-Z0-9]|-(?=[a-zA-Z0-9])){0,38})(?!\w)/
+# Matches PR/issue numbers like "#123".
+GITHUB_ISSUE_REGEXP = /(?<!\w)#(\d+)(?!\w)/
+# Matches commit SHAs (7-40 hex characters).
+GITHUB_COMMIT_REGEXP = /(?<!\w)([0-9a-f]{7,40})(?!\w)/
+
+release_body.gsub!(CLOSING_PARENS_REGEXP) do
+  prefix = $1
+  content = $2
+
+  content.gsub!(GITHUB_USERNAME_REGEXP) do
+    username = $1
+    "[@#{username}](https://github.com/#{username})"
+  end
+
+  content.gsub!(GITHUB_ISSUE_REGEXP) do
+    number = $1
+    "[##{number}](https://github.com/#{owner}/#{repo_name}/pull/#{number})"
+  end
+
+  content.gsub!(GITHUB_COMMIT_REGEXP) do
+    sha = $1
+    "[`#{sha[0, 7]}`](https://github.com/#{owner}/#{repo_name}/commit/#{sha})"
+  end
+
+  "#{prefix}(#{content})"
+end
+
 # Prepare forum post
 title = "#{gem_name} #{version} released"
 
